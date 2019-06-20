b8c0eb130feb1f250f5be853cbda7f5579842f44
Mahnoor Fatima <mahnoor@mahnoors-macbook-air.local>
2019-06-20 13:13:48 +0500

    Initial Commit

---
+++ LifeBoatOrganizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeBoatOrganizer
+//
+//  Created by Mahnoor Fatima on 20/06/2019.
+//  Copyright © 2019 Mahnoor Fatima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
